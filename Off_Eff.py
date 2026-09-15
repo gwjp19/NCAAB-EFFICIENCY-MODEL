2f19fc19698695dbdb8d78819277b6cd279e94b0
gwjp19 <gwjporter@icloud.com>
2026-09-15 12:50:36 -0400

    Update Off_Eff.py

--- Off_Eff.py
+++ Off_Eff.py
@@ -1,1 +1,25 @@
+import sqlite3
 
+conn = splite3.connect("basketball.db")\cursor = conn.cursor()
+
+cursor.execute("""
+  SELECT 
+    game_id,
+    team_id,
+    feild_goals_attempted,
+    offensive_rebounds,
+    total_renounds,
+    turnovers,
+    personal_fouls,
+    feild_goal_attempts_allowed,
+    offensive_rebound_allowed,
+    total_rebounds_allowed,
+    turnovers_forced,
+    fouls_drawn,
+  FROM teamBocscore
+""")
+
+games = cursor.fetchall()
+  
+for game in game_id:
+  
